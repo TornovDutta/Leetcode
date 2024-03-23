29925575e20a55fff16c54ec39d13d7d4123666d
TornovDutta <162907041+TornovDutta@users.noreply.github.com>
2024-03-23 21:54:56 +0530

    Add files via upload

---
+++ array/Easy/Search_Insert_Position.java
@@ -0,0 +1,27 @@
+package Easy;
+
+public class Search_Insert_Position {
+    public static void main(String[] args) {
+        int arr[]={1,3,5,6};
+        int target=5;
+        System.out.println(serachi(arr,target));
+    }
+    static int serachi(int nums[],int target){
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
